0d62dffca45769214e46b3cf2c7b2afc46186c91
Izumrudec <Izumrudec@ya.ru>
2020-04-04 13:03:53 +0300

    test message

--- python/start.py
+++ python/start.py
@@ -1,6 +1,7 @@
 import spaces
 import dots
 if __name__ == "__main__":
+	print("Param pam pam")
 	stringForCleaning = input()
 	stringForCleaning = spaces.strip(stringForCleaning)
 	stringForCleaning = dots.dot(stringForCleaning)
